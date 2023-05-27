278b296c62d7b8e9eea4a2d7374488ab353a1269
josephwamiti <wamiti8711@gmail.com>
2023-05-27 22:37:13 +0300

    add constructor method

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,6 +2,12 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 class TodosContainer extends Component  {
+    constructor(props) {
+        super(props)
+        this.state = {
+            todos: []
+        }
+    }
     render() {
         return (
 
