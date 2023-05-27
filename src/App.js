5a53aab87cce0d7596636c5b6084230960e20162
josephwamiti <wamiti8711@gmail.com>
2023-05-27 22:34:56 +0300

    change functional to class-based component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
-import { TodoContainer } from './components/TodoContainer';
+import React, {Component} from 'react'
+import  TodoContainer from './components/TodoContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="flex justify-center">
-        <h1 className="text-black text-center py-5 font-bold text-xl">Todo List</h1>
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <div className="flex justify-center">
+          <h1 className="text-black text-center py-5 font-bold text-xl">Todo List</h1>
+        </div>
+        <TodoContainer />  
       </div>
-      <TodoContainer />  
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-export const TodoContainer = () => {
-    return (
+class TodosContainer extends Component  {
+    render() {
+        return (
 
-        <div>
-            <div className="p-15">
-                <input className="" type="text" placeholder="task name" maxLength="50" />
-                <input className="" type="text" placeholder="task description" maxLength="50" />
-                <label>
-                    <input
-                    type="checkbox"
-                    // checked={completed}
-                    // onChange={(e) => setCompleted(e.target.checked)}
-                    />
-                    Completed
-                </label>
-                <button type="submit">+</button>
-	        </div>  	    
-	        <div className="listWrapper">
-	            <ul className="taskList">
-	            </ul>
-	        </div>
-        </div>
+            <div>
+                <div className="p-15">
+                    <input className="" type="text" placeholder="task name" maxLength="50" />
+                    <input className="" type="text" placeholder="task description" maxLength="50" />
+                    <label>
+                        <input
+                        type="checkbox"
+                        />
+                        Completed
+                    </label>
+                    <button type="submit">+</button>
+                </div>  	    
+                <div className="listWrapper">
+                    <ul className="taskList">
+                    </ul>
+                </div>
+            </div>
+    
+        )
 
-    )
+    }
+   
 }
+ 
+export default TodosContainer
