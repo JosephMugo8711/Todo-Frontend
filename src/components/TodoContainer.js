8fedd1c7d26113bed6ec44699d9b6d53cbeb9b24
josephwamiti <wamiti8711@gmail.com>
2023-05-27 21:26:05 +0300

    todocontainer component

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -4,7 +4,23 @@ export const TodoContainer = () => {
     return (
 
         <div>
-            
+            <div className="p-15">
+                <input className="" type="text" placeholder="task name" maxLength="50" />
+                <input className="" type="text" placeholder="task description" maxLength="50" />
+                <label>
+                    <input
+                    type="checkbox"
+                    // checked={completed}
+                    // onChange={(e) => setCompleted(e.target.checked)}
+                    />
+                    Completed
+                </label>
+                <button type="submit">+</button>
+	        </div>  	    
+	        <div className="listWrapper">
+	            <ul className="taskList">
+	            </ul>
+	        </div>
         </div>
 
     )
